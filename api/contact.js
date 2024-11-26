7728f17b15f4e20eb7177bc496dda5dc53af1e74
l3onrdo <leomiralli@gmail.com>
2024-11-26 19:26:53 +0100

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -11,11 +11,12 @@ export default async function handler(req, res) {
     }
 
     try {
+    
+        console.log('EMAIL_USER:', process.env.EMAIL_USER);
+        console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
       // Configura il transporter per Nodemailer
       const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
+        host: "gmail",
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
